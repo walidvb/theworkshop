2f2601a68fad01c8a90c65e72ff7ec957f3e40b0
walidvb <walidvb@gmail.com>
2012-03-20 01:45:41 +0100

    added moving stripes

--- cropncombine/CropNCombine.java
+++ cropncombine/CropNCombine.java
@@ -10,50 +10,56 @@ import processing.core.PImage;
 
 
 public class CropNCombine extends PApplet {
-	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	PImage img[] = new PImage[3];
 	PImage render;
 	int totHeight = 450;
 	int totWidth = 450;
 	boolean draw_ = true;
-
+	int loadedID = 0;
+	int step = 0;
+	int r;
+	
 	public void setup() {
+		  frameRate(10);
 
 		size(totWidth, totHeight);
-		String path = "/Users/Gaston/Desktop/proc/";
-		String[] fileNames = listFileNames(path);
-		int loadedID = 0;
-		for(int i = 0; i < fileNames.length; i++)
-		{
-			String fileName =  fileNames[i];
-			println("b4: "+i+" " + fileName);
-
-			if(match(fileName, "((([a-zA-z0-9].)*.(jpeg|gif|jpg)))") != null)
-			{
-				println(loadedID+" "+path + fileName);
-				img[loadedID] = loadImage(path+fileName);
-				++loadedID;
-			}
-		}
+		populate(true);
 	}
 
 	public void draw() {
-
+		
 		if(draw_)
 		{
+
+			//update the img list
+			//populate(true);
 			PImage tmpimg = createImage(totWidth, totHeight, RGB);
 			int n = img.length;
+			int height = totHeight/(n);
 			//loop over img[] and take the 1/n percent of each image
 			for(int i = 0; i < n; i++)
 			{
-
-				int height = totHeight/(n);
-				int offset = i*height;
-
-				tmpimg.copy(img[i], 0, offset, totWidth, height, 0, offset, totWidth, height); 
+				int pos = (i+step)%(n);
+				int offset = pos*height+r;
+				tmpimg.copy(img[i], 0, offset, totWidth, height, 0, offset, totWidth, height);
+				
+				println(step + " pos: " + pos + " offset: " + offset + " tot: " + totHeight);
+				if(offset > totHeight)
+				{
+					//println(step + " pos: " + pos + "offset " + offset + " tot" + totHeight);
+					step = (step+1)%n;
+					r=0;
+				}
 			}
 			image(tmpimg, 0, 0);
-			draw_=false;
+			//something to be done with this... 
+			r+=1;
+			//draw_=false;
 		}
 	}
 
@@ -67,5 +73,22 @@ public class CropNCombine extends PApplet {
 			return null;
 		}
 	}
+	public void populate(boolean reset){
+		String path = "/Users/Gaston/Desktop/proc/";
+		String[] fileNames = listFileNames(path);
+		if(reset)
+			loadedID = 0;
+		for(int i = 0; i < fileNames.length; i++)
+		{
+			String fileName =  fileNames[i];
+
+			if(match(fileName, "((([a-zA-z0-9].)*.(jpeg|gif|jpg)))") != null)
+			{
+				img[loadedID] = loadImage(path+fileName);
+				++loadedID;
+			}
+		}
+	}
+
 }
 
